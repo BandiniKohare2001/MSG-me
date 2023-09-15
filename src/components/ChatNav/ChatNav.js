4617f6430bb3027846fc51276c4977fc854c8690
Bandini2001 <bandinikohare30@gmail.com>
2023-09-15 15:40:16 +0530

    add chat screen

--- src/components/ChatNav/ChatNav.js
+++ src/components/ChatNav/ChatNav.js
@@ -7,8 +7,6 @@ export default function  ChatNav({imgUrl, name, title}){
         <div className="chatnav">
             <img src={imgUrl} alt={title} className="profile-img"/>
             <h2>{name}</h2>
-
-            <h1><i class="fa-solid fa-magnifying-glass"></i></h1>
             <div className="icon-div">
            <img src={Search} alt="search icon" className="icons"/>
            
--- src/components/ChatScreen/ChatScreen.js
+++ src/components/ChatScreen/ChatScreen.js
@@ -1,8 +1,17 @@
-// import import "./ChatScreen.css";
+import "./ChatScreen.css";
+import ChatNav from "../ChatNav/ChatNav"
+import InputBox from "../InputBox/InputBox"
+
 
 export default function  ChatScreen(){
     return(
-        <div>
+        <>
+        <div className="chatsreen">
+        <ChatNav imgUrl="https://pps.whatsapp.net/v/t61.24694-24/375852712_968357537564906_1198883397977467806_n.jpg?ccb=11-4&oh=01_AdRyqnZsnlOtE52wz7hEKelng6Lxquw0HJp4fXJaE6wKJQ&oe=65112A7F&_nc_sid=000000&_nc_cat=111" name="Bandini Kohare" />
+        <div className="message-div"></div>
+        <InputBox />
         </div>
+        </>
+     
     )
 }
