6011b303e24164a84733b55992f4379f76a8b4df
samikshashahare <samikshashahare953@gmail.com>
2023-09-15 16:32:09 +0530

    add  Navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 
 import ReactDOM from 'react-dom/client';
-import Navbar from './component/Navbar/Navbar';
-import Button from './component/Button/Button';
-
-
 import ChatScreen from './components/ChatScreen/ChatScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
