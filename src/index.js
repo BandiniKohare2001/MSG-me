44d54505cf817057e626e39506c6d202f9ea706e
sejal173 <sejalkakade123@gmail.com>
2023-09-16 13:41:22 +0530

    add changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import ChatScreen from './components/ChatScreen/ChatScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
- 
- 
-=======
 <>
 <ChatScreen />
 
@@ -16,7 +12,6 @@ root.render(
 </>
 
 
->>>>>>> 884971781edc51cce72d3cd3fb99abb4d979a0d4
 );
 
-
+    
