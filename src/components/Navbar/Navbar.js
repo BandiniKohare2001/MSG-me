8c857f661e7be85c21dc30ad00d84ac3ff152d75
samikshashahare <samikshashahare953@gmail.com>
2023-09-16 15:01:56 +0530

    modified navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,18 +2,24 @@ import Imgwhatsapp from "./../Navbar/Navbar-Image/whatsapp.png";
 import Imgsearch from "./../Navbar/Navbar-Image/search.png";
 import Imgmenu from "./../Navbar/Navbar-Image/menu.png";
 
-
 import "./Navbar.css";
 
 export default function Navbar() {
-   return (
-      <div className="nav-container">
-
-         <img src={Imgwhatsapp} alt="searchicon" className="img-1" />
-         <div className="menu-item">
-            <img src={Imgsearch} alt="searchicon" className="icon" />
-            <img src={Imgmenu} alt="searchicon" className="icon" />
+    return (
+        <div className="nav-container">
+            <div class="container-1">
+                <div className="logo" >
+                    <img src={Imgwhatsapp} alt="whatsapp-logo" className="img-1" />
+                </div>
+                <div className="menu-item">
+                    <img src={Imgmenu} alt="searchicon" className="icon" />
+                </div>
             </div>
-      </div>
-   )
+            <div className="searchbar">
+                <input class="inputbox"></input>
+                <img src={Imgsearch} alt="searchicon" className="searchicon" />
+            </div>
+        </div>
+
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 
 import ReactDOM from 'react-dom/client';
 import ChatScreen from './components/ChatScreen/ChatScreen';
+import Navbar from './components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <>
+<Navbar/>
 <ChatScreen />
-
-
-
 </>
-
-
 );
 
 
